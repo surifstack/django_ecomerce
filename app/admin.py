a8fbfbeb5d04c51c6a941110c922e35a9b9a240c
= <=>
2022-01-08 04:02:53 -0500

    solve_error_admin-model

--- app/admin.py
+++ app/admin.py
@@ -1,31 +1,27 @@
 from django.contrib import admin
-from .models import ( Customer , Product , Cart , OrderPlaced )
+from app.models import ( Customer , Product , Cart , OrderPlaced )
 
 
 # Register your models here.
 
-@admin.register(Customer)
+admin.site.register(Customer)
 class CustomerModelAdmin(admin.ModelAdmin):
-    can_delete = False
-    verbose_name_plural = 'Customer'
+   
     list_display = ['id','user','name','locality','city','zipcode','state']
 
-@admin.register(Product)
+admin.site.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    can_delete = False
-    verbose_name_plural = 'Product'
+   
     list_display = ['id', 'title','selling_price','discount_price','description','brand','category','product_image']
    
 
-@admin.register(Cart)
+admin.site.register(Cart)
 class CardModelAdmin(admin.ModelAdmin):
-    can_delete = False
-    verbose_name_plural = 'Cart'
+   
     list_display = ['id','user','product','qty']
 
-@admin.register(OrderPlaced)
+admin.site.register(OrderPlaced)
 class OrderPlacedModelAdmin(admin.ModelAdmin):
-    can_delete = False
-    verbose_name_plural = 'OrderPlaced'
+    
     list_display = ['id','user','customer','product','qty','ordered_date','status']
 
